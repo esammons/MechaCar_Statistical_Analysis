049a6008b5e6fadecb95d409ba7d8c2e72b281cc
esammons <ebsammons@gmail.com>
2021-06-27 21:11:29 -0700

    message

---
+++ MechaCarChallenge/MechaCarChallenge(Deliverable3).R
@@ -0,0 +1,8 @@
+library(dplyr)
+mechacarchallenge<- read.csv(file = 'MechaCar_mpg.csv')
+lm(formula = mpg~vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD + mpg,data=mechacarchallenge)
+summary(lm(formula = mpg~vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD + mpg,data=mechacarchallenge))
+Suspension_Coil<- read.csv('Suspension_Coil.csv')
+Total_summary <- Suspension_Coil %>%   summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+Lot_Summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>%   summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups='keep')
+t.test(Suspension_Coil$PSI,mu=1500)
